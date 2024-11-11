1dde14c1d02f4e9ba556bc05208146ef472c0313
Juliana Pereira de Magalhães <61297291+juliana039@users.noreply.github.com>
2024-11-10 21:32:20 -0400

    Initial Commit

---
+++ Timeti_v1Tests/Timeti_v1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Timeti_v1Tests.swift
+//  Timeti_v1Tests
+//
+//  Created by Juliana Pereira de Magalhães on 10/11/24.
+//
+
+import Testing
+@testable import Timeti_v1
+
+struct Timeti_v1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
